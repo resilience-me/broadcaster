9ffad175bd9fc95eac31dfabea3da55af0300f63
resilience-me <emailsareobsolete@gmail.com>
2015-10-13 16:08:03 +0200

    Create broadcast.js

---
+++ js/broadcast.js
@@ -0,0 +1,26 @@
+
+function broadcast() {
+    var privateKey = document.getElementById('privateKey').value
+        
+         var rawTx = document.getElementById('rawTx').value
+        
+        
+
+        privateKey = new Buffer(privateKey, 'hex');
+
+       
+       var tx = new EthTx(JSON.parse(rawTx));
+        tx.sign(privateKey);       
+
+        var serializedTx = tx.serialize().toString('hex');
+        
+
+     var url =   "http://api.etherscan.io/api?module=proxy&action=eth_sendRawTransaction&hex="+serializedTx
+
+
+     $.get(url, function(data, status){
+            alert(JSON.stringify(data));
+        });
+       
+
+}
